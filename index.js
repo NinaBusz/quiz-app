722e7ba7c33741be479b83bd4db635f2266babff
Nina Busz <buss.nina@googlemail.com>
2023-02-09 16:01:15 +0100

    solve-task-one-js

--- index.js
+++ index.js
@@ -2,4 +2,26 @@ console.clear();
 
 //1.
 
+const bookmark = document.querySelector('[data-js="bookmark-button"]');
+
+bookmark.addEventListener("click", () => {
+  bookmark.classList.toggle("question-card__bookmark--remove");
+});
+
+const answerButton = document.querySelector('[data-js="buttonContainer"]');
+const answer = document.querySelector('[data-js="answer"]');
+const showHideToggle = document.querySelector('[data-js="showHideToggle"]');
+let showAnswer = false;
+
+answerButton.addEventListener("click", () => {
+  showAnswer = !showAnswer;
+  answer.classList.toggle("question-card__answer--hidden");
+
+  if (showAnswer) {
+    showHideToggle.textContent = "hide answer";
+  } else {
+    showHideToggle.textContent = "show answer";
+  }
+});
+
 //2.
